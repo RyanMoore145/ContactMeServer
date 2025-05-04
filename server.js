f31f92ba3181338377671282bdbd18cfc99998c7
RyanMoore145 <techdendweller@gmail.com>
2025-05-04 01:40:23 -0300

    added check for null request body, removed dkim

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 
-
 const express = require("express");
 const cors = require("cors");
-
 const nodemailer = require("nodemailer");
-const transporter = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
-	requireTLS: true,
-	from: process.env.SYSTEM_EMAIL,
-	auth: {
-		user: process.env.SYSTEM_EMAIL,
-		pass: process.env.SYSTEM_EMAIL_PASSWORD
-	}
-});
 
 const app = express();
 const port = 3001;
@@ -22,15 +9,31 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
+const transporter = nodemailer.createTransport({
+	host: "smtp.gmail.com",
+	port: 465,
+	secure: true,
+	requireTLS: true,
+	auth: {
+		user: process.env.SYSTEM_EMAIL,
+		pass: process.env.SYSTEM_EMAIL_PASSWORD
+	}
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});	
+
+
 function validateEmail(email) {
 	return email.match(
 		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	);
 }
 
-async function sendEmail(senderEmail, emailHeader, emailBody) {
+function sendEmail(senderEmail, emailHeader, emailBody) {
 	let mailOptions = {
-		from: `Ryan Moore <${process.env.SYSTEM_EMAIL}>`,
+		from: `${process.env.SYSTEM_EMAIL}`,
 		to: senderEmail,
 		subject: emailHeader,
 		text: emailBody
@@ -48,6 +51,10 @@ async function sendEmail(senderEmail, emailHeader, emailBody) {
 
 app.post("/", (req, res) => {
 	console.log(req.body);
+	if (!req.body) {
+		res.status(400).send({error: "Body empty"});
+		return;
+	}
 	const {senderEmail, emailHeader, emailBody} = req.body;
 	if (
 		!senderEmail ||
@@ -55,16 +62,15 @@ app.post("/", (req, res) => {
 		!emailBody
 	) {
 		res.status(400).send({error: "Not all fields included"});
+		return;
 	}
 	else if (!validateEmail(req.body.senderEmail)) {
 		res.status(400).send({error: "Invalid email format"});
+		return;
 	}
 	else {
 		sendEmail(senderEmail, emailHeader, emailBody);
 		res.status(200).end();
+		return;
 	}
 });
-
-app.listen(port, () => {
-	console.log(`listening on port ${port}`);
-});	
